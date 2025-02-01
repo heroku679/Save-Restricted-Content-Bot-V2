55cb02cb0f4b749dba4dbbd5c7bfad2d50f7c618
heroku679 <heroku1234567@outlook.com>
2025-02-01 17:13:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "12380656"))
+API_HASH = getenv("API_HASH", "d927c13beaaf5110f25c505b7c071273")
+BOT_TOKEN = getenv("BOT_TOKEN", "7363815658:AAFgCXWm96WM-qP0wvbMzmYz4oxIFmcmNRk")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7825510422").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://sunnylona:jUmDmuPK98tyB1T9@cluster0.tonrs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002441594756")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002270567487"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
